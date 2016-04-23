922b57646a9523df6444eca8ef304b4e94a523f4
Stuart Feichtinger <stuart.feichtinger@gmail.com>
2016-04-23 16:25:30 -0500

    Fixed upstream rebase

--- util.py
+++ util.py
@@ -28,7 +28,6 @@ class VersionData:
 
         elif(string is not None):
             self.string = string
-            print(string)
             self._loadDict(self._makeDict(self.string))
 
         else:
@@ -73,10 +72,10 @@ def parseMechJeb(r):
 
 def syncUpstream(repPath, lBranch="master", uBranch="MuMech"):
 
-    subprocess.check_output(["git", "-C", repPath, "fetch", uBranch])
+    subprocess.check_output(["git", "-C", repPath, "fetch", "upstream"])
     subprocess.check_output(["git", "-C ", repPath, "checkout", lBranch])
     subprocess.check_output(
-        ["git", "-C", repPath, "rebase", uBranch + "/" + lBranch])
+        ["git", "-C", repPath, "rebase", "upstream" + "/" + lBranch])
     subprocess.check_output(["git", "-C", repPath, "push", "origin", lBranch])
 
 
